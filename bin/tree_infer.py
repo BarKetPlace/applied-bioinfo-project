43ff75b31761dd92195955cfade8c00e8f1cc20e
Antoine Honoré <honore@kth.se>
2019-09-10 18:25:16 +0200

    Fix the format pb

--- bin/tree_infer.py
+++ bin/tree_infer.py
@@ -1,33 +1,31 @@
 import argparse
-import sys
-from Bio import Phylo
-from Bio import SeqIO
 from Bio import AlignIO
-from Bio.Phylo.TreeConstruction import DistanceCalculator
-from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
+from Bio.Phylo.TreeConstruction import DistanceCalculator,DistanceTreeConstructor
 
-parser = argparse.ArgumentParser(description = 'Create a dendrogram from a trimmed .msl alignmnet')
-parser.add_argument('file_pth', type = str, help = 'path to the .msl file')
-args = parser.parse_args()
-file = args.file_pth
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create a dendrogram from a trimmed .msl alignmnet')
+    parser.add_argument('file_path', type=str, help='path to the .msl file')
+    args = parser.parse_args()
+    fname = args.file_path
 
-SeqIO.convert(file,"fasta","temp.phy", "phylip")
-# read the sequences in phylip format and align
-align = AlignIO.read('temp.phy', 'phylip')
+    #SeqIO.convert(fname, "fasta", "temp.phy", "phylip")
+    # read the sequences in phylip format and align
+    #align = AlignIO.read('temp.phy', 'phylip')
 
-# Print the alignment
-#print(align)
+    align = AlignIO.read(fname,'fasta')
 
-#calculate distance matrix
-distm = DistanceCalculator('identity').get_distance(align)
+    # Print the alignment
+    #print(align)
 
-#print out the distance Matrix
-print('\nDistance Matrix',file=sys.stderr)
-print(distm,file=sys.stderr)
+    #calculate distance matrix
+    distm = DistanceCalculator('identity').get_distance(align)
 
-#calculate the dendrogam using UPGMA algorithm
-tree = DistanceTreeConstructor().upgma(distm)
+    #print out the distance Matrix
+    #print('\nDistance Matrix', file=sys.stderr)
+    #print(distm, file=sys.stderr)
 
-#print out he dendrogram
-print('\nDendrogram',file=sys.stderr)
-Phylo.draw_ascii(tree)
+    #calculate the dendrogam using UPGMA algorithm
+    tree = DistanceTreeConstructor().upgma(distm)
+
+    #print out he dendrogram
+    print(tree.format('newick'))
